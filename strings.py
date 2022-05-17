2c6cc3460c6e5511fdf473e6f7b062e9ffdbd78d
StephanieHeesen <105100065+StephanieHeesen@users.noreply.github.com>
2022-05-17 17:10:06 +0200

    Add files via upload

--- strings.py
+++ strings.py
@@ -4,23 +4,23 @@ __human_name__ = 'strings'
 
 # Add your code after this line
 
-goal1gescoord = 'Ruud Gullit '
-goal2gescoord = 'Marco van Basten '
+goal1gescoord = 'Ruud Gullit'
+goal2gescoord = 'Marco van Basten'
 
 #minuten dat de goals gemaakt zijn.
 goal_0 = 32
 goal_1 = 54
 
-scorers = (goal1gescoord + str(goal_0) + ", " + goal2gescoord + str(goal_1) )
+scorers = (goal1gescoord + ' ' + str(goal_0) + ", " + goal2gescoord + ' ' + str(goal_1) )
 
-report = f'{goal1gescoord}scored in the {str(goal_0)}nd minute\n{goal2gescoord}scored in the {str(goal_1)}th minute'
+report = f'{goal1gescoord} scored in the {goal_0}nd minute\n{goal2gescoord} scored in the {goal_1}th minute'
 
 player = 'Hans van Breukelen'
-first_name = player[player.find("Hans"):4] 
-last_name_len = len(player[player.find("van Breukelen"):])
-name_short = player[0]+ '. '+ player[5:] 
-chant = f'{first_name}! '*3 + f"{first_name}!"
-good_chant = chant != len('Hans! ')*4
+first_name = player[:player.find(" ")] 
+last_name_len = len(player[player.find(" "):])
+name_short = player[0]+ '.'+ player[player.find(" "):] 
+chant = f'{first_name}! '*(len(first_name)-1) + f'{first_name}!'
+good_chant = chant[-1] != " "
 
 
 
